33d108aa06709810e256623e0b437330da2f2b7a
Đăng Châu <chauhaidang1@gmail.com>
2019-04-07 14:48:23 +0700

    Protractor training part 2

--- conf.js
+++ conf.js
@@ -1,4 +1,27 @@
 exports.config = {
+    framework: 'jasmine',
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true, // Use colors in the command line report.
+    },
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        browserName: 'chrome'
+    },
+
+    // Spec patterns are relative to the configuration file location passed
+    // to protractor (in this example conf.js).
+    // They may include glob patterns.
+    specs: ['spec.js'],
+
+    //Set time out for all script
+    allScriptsTimeout: 9999999999,
+    
+    //Set up before starting tests
+    onPrepare: function(){
+        browser.ignoreSynchronization = true
+        browser.waitForAngularEnabled(false)
+    }
 };
--- spec.js
+++ spec.js
@@ -1,10 +1,19 @@
 describe('Enter name feature', function(){
-    it('should enter name as Tom',function(){
-        browser.get('https://angularjs.org');
-        element(by.model('yourName')).sendKeys('Lucy')
+    it('should enter name as Tom',async function(){
+        await browser.get('https://angularjs.org');
+        await element(by.model('yourName')).sendKeys('Lucy')
 
         var text = element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
-        expect(text.getText()).toEqual('Hello Lucy!');
-        browser.sleep(5000);
+        expect(await text.getText()).toEqual('Hello Lucy!');
+        await browser.sleep(5000);
+        
+    });
+});
+
+// Using async await pattern to force javascript run in order.
+describe('free crm title test', function(){
+    it('get title test', async function(){
+        await browser.get('https://www.google.com');
+        expect(await browser.getTitle()).toEqual('Google');
     });
 });
