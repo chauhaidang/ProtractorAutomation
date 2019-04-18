7e730d24429b104df77ca9dca582d3da9a036486
chauhaidang <chauhaidang1@gmail.com>
2019-04-18 14:51:44 +0700

    PA4 completed (#5)

--- spec.js
+++ spec.js
@@ -67,3 +67,35 @@ describe('multiple scenarios', function(){
         expect(firstNumber.getAttribute('value')).toEqual('5');        
     });
 });
+
+describe('get list of elements', function() {
+    // global var
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var goButton = element(by.id('gobutton'));
+    var result = element(by.className('ng-binding'));
+    var history = element.all(by.repeater('result in memory'));
+
+    beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+
+    function add(a, b) {
+        firstNumber.sendKeys(a);
+        secondNumber.sendKeys(b);
+        goButton.click();
+    }
+
+    it('check the history', function() {
+        add(1,2);
+        add(2,3);
+        add(3,4);
+
+        expect(history.count()).toEqual(3);
+
+        add(4,5);
+        add(5,6);
+
+        expect(history.count()).toEqual(5);
+    })
+});
