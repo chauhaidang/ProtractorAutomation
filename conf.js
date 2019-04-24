c7b27cbc57fcfafbe3c5975f10b40ea06c937361
ChauHaiDang1 <chauhaidang1@gmail.com>
2019-04-24 16:35:45 +0700

    updatre

--- conf.js
+++ conf.js
@@ -1,18 +1,11 @@
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
 exports.config = {
     framework: 'jasmine',
     jasmineNodeOpts: {
-        //If true, print colors to the terminal.
-        showColors: true,
-        // Default time to wait in ms before a test fails.
         defaultTimeoutInterval: 30000,
-        // Function called to print jasmine results.
-        print: function () { },
-        // If set, only execute specs whose names match the pattern, which is
-        // internally compiled to a RegExp.
-        grep: 'pattern',
-        // Inverts 'grep' matches
-        invertGrep: false
-},
+    },
     seleniumAddress: 'http://localhost:4444/wd/hub',
         specs: ['spec.js'],
             multiCapabilities: [
